38a4e80e7c0c977f461cd80255a1cde77bd2781c
Mark Kelly <G00364705@gmit.ie>
2019-12-18 21:18:09 +0000

    updated DAO

--- patient_DAO.py
+++ patient_DAO.py
@@ -1,12 +1,14 @@
 import mysql.connector
+import dbconfig as cfg
+
 class PatientDAO:
 	db=""
 	def __init__(self):
 		self.db = mysql.connector.connect(
-		host="localhost",
-		user="root",
-		password="root",
-		database="patientdb"
+		host=cfg.mysql['host'],
+		user=cfg.mysql['username'],
+		password=cfg.mysql['password'],
+		database=cfg.mysql['database']
 		)
 	def create (self, values):
 		cursor = self.db.cursor()
